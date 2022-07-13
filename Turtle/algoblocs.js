ceef7cde4412ce71ebad702088802031e4cd51e5
m4j0ra <ismael.benelhadj@hotmail.com>
2022-07-13 12:33:58 +0200

    Ajout cours algo

---
+++ Turtle/algoblocs.js
@@ -0,0 +1,929 @@
+
+/*
+// LVL 1
+setPos (300,300);
+setLineWidth(10);
+changeColor(color.red);
+
+// Réalisation A1
+faceDown();
+forward(200);
+faceRight();
+forward(100);
+*/
+/*
+
+// LVL 2
+setPos (300,300);
+setLineWidth(10);
+changeColor(color.yellow)
+faceLeft();
+forward(100);
+faceDown();
+forward(100);
+faceRight();
+forward(200);
+faceUp();
+forward(200);
+*/
+/*
+
+// LVL 3
+setPos (300,300);
+setLineWidth(10);
+changeColor(color.#d35400)
+faceLeft();
+forward(100);
+faceRight();
+forward(200);
+faceLeft();
+forward(100);
+faceDown();
+forward(100);
+faceUp();
+forward(200);
+*/
+/*
+
+// LVL 4
+setPos (300,300);
+setLineWidth(10);
+changeColor(color.green)
+faceRight();
+forward(100);
+arcLeft(50,180);
+faceLeft();
+forward(50);
+arcRight(50,180);
+faceRight();
+forward(100);
+*/
+
+/*
+// LVL 5
+setPos (300,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+arcLeft(80,360);
+faceLeft();
+arcRight(40,180);
+arcLeft(40,180);
+*/
+
+/*
+// LVL 6
+setPos (300,400);
+setLineWidth(10);
+changeColor("#00FFFF");
+faceUp();
+forward(50);
+up();
+forward(50);
+down();
+forward(100);
+*/
+
+/*
+// LVL 7
+setPos (250,300);
+setLineWidth(10);
+changeColor(color.green);
+faceDown();
+forward(100);
+arcLeft(50,180)
+forward(50);
+up();
+forward(50);
+down();
+forward(100);
+arcLeft(50,180);
+forward(50);
+*/
+
+/*
+// LVL 8
+setPos (250,300);
+setLineWidth(10);
+changeColor(color.red);
+faceUp();
+forward(50);
+faceRight();
+forward(100);
+faceDown();
+forward(200);
+faceLeft();
+forward(50);
+up();
+forward(50);
+faceUp();
+forward(50);
+down();
+forward(50);
+*/
+
+// LVL 9
+
+/*
+setPos (200,400);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+forward(200);
+faceLeft();
+arcRight(50,180);
+arcLeft(50,180);
+forward(200);
+arcLeft(50,180);
+arcRight(50,180);
+*/
+
+/*
+// LVL 10
+setPos (300,300);
+setLineWidth(10);
+changeColor(color.red);
+faceRight();
+arcLeft(50,180);
+changeColor(color.green);
+faceLeft();
+arcLeft(100,180);
+changeColor(color.yellow);
+faceRight();
+arcLeft(200,180);
+*/
+
+/*
+// LVL 11
+setPos (150,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceUp(),
+arcRight(50,180);
+faceRight();
+up();
+forward(50);
+down();
+faceUp();
+arcRight(50,180);
+up();
+changeColor(color.red)
+faceLeft();
+forward(50);
+faceDown();
+forward(50);
+down();
+arcRight(75,180);
+*/
+
+/*
+// LVL 12
+setPos (50,250);
+setLineWidth(10);
+changeColor(color.green);
+faceUp();
+arcRight(50,180);
+faceDown();
+forward(100);
+arcLeft(50,180);
+up();
+faceRight();
+forward(50);
+changeColor("#d35400");
+down();
+faceDown();
+arcLeft(50,180);
+faceUp();
+forward(100);
+arcRight(50,180);
+*/
+
+
+
+
+//             PASSAGE LVL B                 
+
+/*
+// LVL 1
+setPos(150,200);
+setLineWidth(10);
+changeColor(color.green)
+
+for(let i = 0 ; i < 4 ; i++) {
+faceRight();
+forward(50);
+faceDown();
+forward(50);
+}
+*/
+
+/*
+// LVL 2
+setPos(50,300);
+setLineWidth(10);
+changeColor(color.red);
+for(let i = 0 ; i < 4 ; i++) {
+    faceUp();
+    forward(50);
+    faceRight();
+    forward(50);
+    faceDown();
+    forward(50);
+    faceRight();
+    forward(50);
+}
+*/
+
+/*
+// LVL 3
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+for(let i = 0 ; i < 4 ; i++) {
+    right(90);
+    forward(100);
+}
+*/
+/*
+// LVL 4
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+for(let i = 0 ; i < 4 ; i++) {
+    left(180 -60)
+    forward(100)
+}
+*/
+
+// LVL 5
+
+/*
+setPos(300,450);
+setLineWidth(10);
+changeColor("#d35400");
+faceUp();
+forward(200);
+changeColor(color.red)
+for (let i = 0 ; i < 4 ; i++) {
+right(120);
+forward(50);
+}
+*/
+
+/*
+// LVL 6
+setPos(150,300);
+setLineWidth(10);
+changeColor(color.red);
+faceRight();
+for (let i = 0 ; i < 2 ; i++) {
+    faceRight();
+    forward(50);
+    faceUp();
+    forward(50);
+}
+changeColor(color.green)
+for (let i = 0 ; i < 3 ; i++) {
+    faceRight();
+    forward(50);
+    faceDown();
+    forward(50);
+}
+*/
+
+/*
+// LVL 7
+setPos(300,300);
+setLineWidth(10);
+changeColor("#d35400");
+faceRight();
+for (let i = 0 ; i < 4 ; i++) {
+forward (100);
+right(90);
+}
+faceRight();
+for (let i = 0 ; i < 3 ; i++) {
+    forward(100);
+    left(120);
+
+}
+*/
+/*
+// LVL 8
+setPos(250,300);
+setLineWidth(10);
+changeColor(color.yellow);
+faceRight();
+for (let i = 0 ; i < 8 ; i++) {
+    forward(200);
+    right(180-45);
+}
+*/
+
+
+/*
+// LVL 9
+setPos(150,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+forward(50);
+up();
+forward(50);
+down();
+for (let i = 0 ; i < 3 ; i++) {
+    forward(100);
+    right(120);
+}
+*/
+/*
+// LVL 10
+setPos(50,300);
+setLineWidth(10);
+changeColor("#34ebeb");
+faceRight();
+for (let i = 0 ; i < 8 ; i++) {
+    forward(100);
+    right(180 - 45);
+}
+up();
+forward(400);
+down();
+changeColor(color.yellow)
+for (let i = 0 ; i < 8 ; i++) {
+    forward(150);
+    left(135);
+}
+faceDown();
+up();
+forward(100);
+down();
+changeColor("#e038b4");
+for (let i = 0 ; i < 8 ; i++) {
+    forward (100);
+    right(135);
+}
+*/
+
+
+/*
+// LVL 11
+
+setPos(150,300);
+setLineWidth(10);
+changeColor(color.red);
+faceRight();
+for( let i = 0 ; i < 4 ; i++ ) {
+forward(100);
+left(90);
+forward(100);
+right(90);
+forward(100);
+right(90);
+}
+
+*/
+
+/*
+// LVL 12
+
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceDown();
+for (let i = 0 ; i < 3 ; i++) {
+    forward(100);
+    right(90);
+}
+faceUp();
+up();
+forward(100);
+down();
+changeColor(color.green)
+for (let i = 0 ; i < 3 ; i++){
+    forward(100);
+    right(90);
+}
+*/
+
+/*
+// LVL 13
+
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.green);
+faceRight();
+for(let i = 0 ; i < 8 ; i++) {
+    forward(100);
+    left(135);
+    forward(50);
+    right(90);
+    forward(50);
+    right(90);
+}
+*/
+
+/*
+// LVL 14
+
+setPos(100,200);
+setLineWidth(10);
+changeColor(color.yellow);
+faceRight();
+forward(50);
+for ( let i = 0 ; i < 4 ; i++) {
+right(120);
+forward(100);
+left(120);
+forward(100);
+}
+right(120);
+forward(100);
+*/
+
+/*
+// LVL 15
+setPos(200,500);
+setLineWidth(10);
+changeColor(color.red);
+faceRight();
+for(let i = 0 ; i < 4 ; i++) {
+    forward(100);
+    arcLeft(100,90);
+}
+
+
+*/
+
+
+/*
+// LVL 16
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.green);
+faceRight();
+arcLeft(200,90);
+faceLeft();
+for(let i = 0 ; i < 4 ; i++) {
+    arcLeft(25,90);
+    arcRight(25,90);
+}
+*/
+
+/*
+// LVL 17
+setPos(150,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+for(let i = 0 ; i < 6 ; i++) {
+    arcLeft (100,180);
+    left (120);
+}
+*/
+
+// LVL 18
+/*
+setPos(200,400);
+setLineWidth(10);
+changeColor(color.yellow);
+faceRight();
+for(let i = 0 ; i < 8 ; i++) {
+    forward(200);
+    left(90);
+    forward(50);
+    left(45);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// LVL C
+
+/*
+// LVL 1
+setPos(50,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+
+for(let i = 0 ; i < 4 ; i++) {
+    forward(100);
+    for (let a = 0 ; a < 3 ; a++) {
+        right(120);
+        forward(100);
+    }
+}
+*/
+
+/*
+// LVL 2
+setPos(300,580);
+setLineWidth(10);
+changeColor(color.red);
+faceUp();
+for(let i = 0 ; i < 5 ; i++) {
+    for(let j = 0 ; j < 4 ; j++) {
+        forward(50);
+        right(90);
+    }
+    up();
+    forward(100);
+    down();
+}
+*/
+
+/*
+// LVL 3
+setPos(50,300);
+setLineWidth(10);
+shiftColor(0.3);
+faceRight();
+for(let i = 0 ; i < 3 ; i++) {
+    for(let j = 0 ; j < 8 ; j++) {
+        forward(100);
+        left(180-45);
+    }
+    up();
+    forward(200);
+    down();
+    shiftColor(0.2);
+}
+*/
+
+/*
+// LVL 4
+setPos(20,300);
+setLineWidth(10);
+changeColor(color.red);
+faceRight();
+for(let i = 0 ; i < 5 ; i++) {
+    for(let j = 0 ; j < 3 ; j++){
+        forward(50);
+        left(120);
+    }
+    changeColor(color.blue)
+    for(let k = 0 ; k < 4 ; k++) {
+        forward(50);
+        right(90);
+    }
+    up();
+    forward(100);
+    down();
+    changeColor(color.red)
+}
+*/
+
+/*
+// LVL 5
+setPos(150,300);
+setLineWidth(10);
+shiftColor(0.2);
+faceRight();
+for(let i = 0 ; i < 4 ; i++){
+    for(let j = 0 ; j < 8 ; j++) {
+        forward(200);
+        left(180-45);
+    }
+    shiftColor(0.20);
+    right(90);
+}
+*/
+
+// LVL 6
+
+/*
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.yellow);
+faceRight();
+for(let i = 0 ; i < 3 ; i++) {
+    for(let j = 0 ; j < 3 ; j++) {
+        forward(100);
+        left(120);
+    }
+    forward(100);
+    right(120);
+
+}
+*/
+/*
+// LVL 7
+setPos (300,300);
+setLineWidth(10);
+changeColor(color.red);
+faceRight();
+for(let i = 0 ; i < 8 ; i++) {
+    for(let j = 0 ; j < 3 ; j++) {
+        forward(50);
+        right(120);
+    }
+    forward(50);
+    left(45);
+}
+*/
+// LVL 8
+/*
+setPos(300,600);
+setLineWidth(10);
+changeColor(color.green);
+faceUp();
+forward(300);
+for(let i = 0 ; i < 4 ; i++) { 
+    for(let j = 0 ; j < 3 ; j++) {
+    changeColor(color.red);
+    right(120);
+    forward(100);
+    }
+}
+// EXERCICE PAS FINI ^^^^^^ //
+*/
+
+
+// LVL D
+
+// LVL 1
+
+
+/*
+setPos(250,150);
+setLineWidth(10);
+changeColor(color.red);
+faceRight();
+let a = 50;
+let b = 45;
+for(let i = 0 ; i < 8 ; i++){
+    forward(a);
+    right(b);
+    forward(a);
+}
+*/
+
+
+// LVL 2
+/*
+setPos(200,300);
+setLineWidth(10);
+changeColor(color.green);
+faceRight();
+let a = 50;
+let b = 100;
+let c = 90;
+forward(a);
+right(c);
+forward(b);
+left(c);
+forward(b + b);
+left(c);
+forward(a);
+left(c);
+forward(b);
+forward(a);
+*/
+/*
+// LVL 3
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+let a = 100;
+let b = 135;
+forward(a+a);
+right(b);
+forward(250);
+left(b);
+forward(a+a);
+*/
+/*
+// LVL 4
+setPos(100,580);
+setLineWidth(10);
+changeColor(color.blue);
+faceUp();
+let a = 100;
+let b = 50;
+let c = faceUp;
+let d = faceRight;
+let e = faceLeft;
+let u = forward;
+let f = 25;
+
+for(let i = 0 ; i < 6 ; i++){
+    c();    
+    u(b);
+    e();
+    u(b);
+    c();
+    u(f);
+    d();
+    u(a);
+}
+*/
+/*
+// LVL 9
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+let a = 50;
+let b = 120;
+for(let i = 0 ; i < 20 ; i++){
+    forward(a);
+    right(b);
+    a += 50;
+}
+*/
+/*
+// LVL 10
+setPos(300,300);
+setLineWidth(4);
+changeColor(color.yellow);
+faceRight();
+let line = 50;
+let angle = 90;
+for(let i = 0 ; i < 10 ; i++){
+    forward(line);
+    right(angle);
+    line +=50;
+}
+*/
+/*
+// LVL 11
+setPos(10,300);
+setLineWidth(10);
+changeColor(color.back);
+faceRight();
+let a = 100
+let b = 120
+
+for(let i = 0 ; i < 5 ; i++){
+    for(let j = 0 ; j < 3 ; j++){
+        forward(a);
+        left(b);
+    }
+    forward(a);
+    a -=20;
+}
+*/
+
+// LVL 12
+/*
+setPos(300,300);
+setLineWidth(10);
+changeColor(color.blue);
+faceRight();
+let a = 50;
+let b = 120;
+let c = 60;
+for(let i = 0 ; i < 6 ; i++){
+    for(let j = 0 ; j < 3 ; j++){
+        forward(a);
+        left(b);
+    }
+    forward(a);
+    left(c);
+    a += 50;
+}
+*/
+
+// LVL 13
+/*
+setPos(350,300);
+setLineWidth(7);
+changeColor(color.red);
+faceRight();
+let a = 50
+for(let i = 0 ; i < 8 ; i++){
+    for(let j = 0 ; j < 1 ; j++){
+        arcRight(a,180);
+    }
+    up();
+    forward(50);
+    down();
+    a +=25;
+}
+*/
+/*
+// LVL 14
+setPos(50,300);
+setLineWidth(10);
+changeColor(color.green);
+left(135);
+for (let i = 0 ; i < 2 ; i++){
+    let a = 25;
+    for(let j = 0 ; j < 4 ; j++){
+        forward(a);
+        right(90);
+        forward(a);
+        left(90);
+        a +=25;
+    }
+    right(90);
+    right(90);
+}
+*/
